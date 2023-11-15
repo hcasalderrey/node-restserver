36eea888f906177e0abb93e407c0d3dc23dcb512
HCasalderrey <el_topo_48@hotmail.com>
2023-11-15 20:43:06 -0300

    final seccion 10

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -69,9 +69,9 @@ const usuariosPost =  async (req, res= response) => {
 }
 const usuariosDelete = async (req, res= response) => {
 
-    const id = req.params.id;
+    const {id} = req.params;
     const usuario = await Usuario.findByIdAndUpdate(id,{estado: false})
-    res.json(usuario)
+    res.json({usuario})
    
 }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,8 +2,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
+const { validarCampos,validarJWT,esAdminRole, tieneRole } = require('../middlewares');
 
-const { validarCampos } = require('../middlewares/validar-campos');
 const { esRolValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const { usuariosGet,
@@ -34,6 +34,9 @@ router.post('/',[
 ], usuariosPost );
 
 router.delete('/:id',[
+    validarJWT,
+    //esAdminRole,
+    tieneRole('ADMIN_ROLE','VENTAS_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom( existeUsuarioPorId ),
     validarCampos
